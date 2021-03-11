2983b9bc7fe99ae3e5bb8d4c22e9c9b646790831
Everton Cabral <evertoncab@gmail.com>
2021-03-11 16:09:31 -0300

    #04 ReactJS para Iniciantes - React router e absolute imports - Vinicicius Dacal

--- new-app/src/components/Promotion/Card/Card.js
+++ new-app/src/components/Promotion/Card/Card.js
@@ -3,7 +3,11 @@ import "./Card.css";
 
 const PromotionCard = ({ promotion }) => (
   <div className="promotion-card">
-    <img src={promotion.imageUrl} className="promotion-card__image" />
+    <img
+      src={promotion.imageUrl}
+      className="promotion-card__image"
+      alt={promotion.title}
+    />
     <div className="promotion-card__info">
       <h1 className="promotion-card__title">{promotion.title}</h1>
       <span className="promotion-card__price">R$ {promotion.price}</span>
@@ -20,6 +24,7 @@ const PromotionCard = ({ promotion }) => (
         <a
           href={promotion.url}
           target="_blank"
+          rel="noopener noreferrer"
           className="promotion-card__link"
         >
           Ir para o site
