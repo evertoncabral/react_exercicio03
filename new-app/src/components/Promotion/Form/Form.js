3a043cb9b9faf60a0afda3436a979b68b137beba
Everton Cabral <evertoncab@gmail.com>
2021-03-12 20:34:17 -0300

    #08 ReactJS para Iniciantes - Implementando um formulário de edição

--- new-app/src/components/Promotion/Form/Form.js
+++ new-app/src/components/Promotion/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Form.css";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
@@ -9,9 +9,17 @@ const initialValue = {
   imageUrl: "",
   price: "",
 };
-const PromotionForm = () => {
-  const [values, setValues] = useState(initialValue);
+const PromotionForm = ({ id }) => {
+  const [values, setValues] = useState(id ? null : initialValue);
   const history = useHistory();
+
+  useEffect(() => {
+    if (id) {
+      axios.get(`http://localhost:5000/promotions/${id}`).then((response) => {
+        setValues(response.data);
+      });
+    }
+  }, []);
 
   function onChange(event) {
     const { name, value } = event.target;
@@ -21,43 +29,69 @@ const PromotionForm = () => {
   function onSubmit(event) {
     event.preventDefault();
 
-    axios
-      .post("http://localhost:5000/promotions?_embed=comments&_order=desc&_sort=id", values)
-      .then((response) => {
-        history.push("/");
-      });
+    const method = id ? "put" : "post";
+    const url = id
+      ? `http://localhost:5000/promotions/${id}`
+      : `http://localhost:5000/promotions`;
+
+    axios[method](url, values).then((response) => {
+      history.push("/");
+    });
   }
 
   return (
     <div>
       <h1>Promotion Form</h1>
       <h2>Nova promoção</h2>
-      <form onSubmit={onSubmit}>
-        <div className="propotion-form__group">
-          <label htmlfor="title">Título</label>
-          <input id="title" name="title" type="text" onChange={onChange} />
-        </div>
-        <div className="propotion-form__group">
-          <label htmlfor="url">Link</label>
-          <input id="url" name="url" type="text" onChange={onChange} />
-        </div>
-        <div className="propotion-form__group">
-          <label htmlfor="imageUrl">Imagem (URL)</label>
-          <input
-            id="imageUrl"
-            name="imageUrl"
-            type="text"
-            onChange={onChange}
-          />
-        </div>
-        <div className="propotion-form__group">
-          <label htmlfor="price">Preço</label>
-          <input id="price" name="price" type="number" onChange={onChange} />
-        </div>
-        <div>
-          <button type="submit">Salvar</button>
-        </div>
-      </form>
+      {!values ? (
+        <div>Carregando......</div>
+      ) : (
+        <form onSubmit={onSubmit}>
+          <div className="propotion-form__group">
+            <label htmlfor="title">Título</label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              onChange={onChange}
+              value={values.title}
+            />
+          </div>
+          <div className="propotion-form__group">
+            <label htmlfor="url">Link</label>
+            <input
+              id="url"
+              name="url"
+              type="text"
+              onChange={onChange}
+              value={values.url}
+            />
+          </div>
+          <div className="propotion-form__group">
+            <label htmlfor="imageUrl">Imagem (URL)</label>
+            <input
+              id="imageUrl"
+              name="imageUrl"
+              type="text"
+              onChange={onChange}
+              value={values.imageUrl}
+            />
+          </div>
+          <div className="propotion-form__group">
+            <label htmlfor="price">Preço</label>
+            <input
+              id="price"
+              name="price"
+              type="number"
+              onChange={onChange}
+              value={values.price}
+            />
+          </div>
+          <div>
+            <button type="submit">Salvar</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
